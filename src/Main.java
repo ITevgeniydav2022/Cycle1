da71f3dc8ec3bd9653510c327b3ae53b64b1ac52
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-13 19:27:10 +0300

    Проверте ДЗ Циклы ч.1 Задания 1,2,3,4,5,6,7,8,9. Отредактированное!

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         // Задание 2
         System.out.println("Задание 2");
-        for (int i = 10; i > 0; i--) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
         // Задание 3
         System.out.println("Задание 3");
-        for (int i = 0; i < 17; i = i + 2 ) {
+        for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
         // Задание 4
@@ -22,35 +22,41 @@ public class Main {
         }
         // Задание 5
         System.out.println("Задание 5");
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(i);
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + " Год является високосным. ");
+            }
         }
-        // Задание 6
-        System.out.println("Задание 6");
-        for (int i = 7; i < 100; i = i + 7) {
-            System.out.println(i);
-        }
-        // Задание 7
-        System.out.println("Задание 7");
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
-        }
-        // Задание 8
-        System.out.println("Задание 8");
-        int salary = 29000;
-        int total = 0;
-        for (int i = 1; i <= 12; i++) {
-            total = total + salary;
-            System.out.println(" Месяц " + i + ", сумма накоплений равна " + total );
-        }
-        // Задание 9
-        System.out.println("Задание 9");
-        int salary1 = 29000;
-        int total1 = 0;
-        for (int i = 1; i <= 12; i++) {
-            total1 = total1 + total1 * 12 / 100;
-            total1 = total1 + salary1;
-            System.out.println(" Месяц " + i + ", сумма накоплений равна " + total1 );
+            // Задание 6
+            System.out.println("Задание 6");
+            for (int i = 7; i < 100; i += 7) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+            // Задание 7
+            System.out.println("Задание 7");
+            for (int i = 1; i <= 512; i = i * 2) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+            // Задание 8
+            System.out.println("Задание 8");
+            int salary = 29000;
+            int total = 0;
+            for (int i = 1; i <= 12; i++) {
+                total = total + salary;
+                System.out.println(" Месяц " + i + ", сумма накоплений равна " + total);
+            }
+            // Задание 9
+            System.out.println("Задание 9");
+            int salary1 = 29000;
+            int total1 = 0;
+            for (int i = 1; i <= 12; i++) {
+                total1 = total1 + total1/100;
+                total1 = total1 + salary1;
+                System.out.println(" Месяц " + i + ", сумма накоплений равна " + total1);
+            }
         }
     }
-}
